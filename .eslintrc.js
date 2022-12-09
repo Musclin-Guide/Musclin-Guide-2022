52f82e7da58ff98926702c345d3e14bc9dbfe007
Ji-yeon CHOI <jurneyx2@gmail.com>
2022-12-09 18:20:34 +0900

    [chore,setting] tailwindCss 설치 및 config파일 수정, Eslint 설치

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/jsx-runtime',
+    'next/core-web-vitals',
+  ],
+  plugins: ['react', '@typescript-eslint', 'react-hooks'],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+}
