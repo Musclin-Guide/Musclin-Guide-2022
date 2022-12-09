46f8fb6046c856e8c446731de82bc27f62278ff8
Ji-yeon CHOI <jurneyx2@gmail.com>
2022-12-09 19:21:13 +0900

    [chore,setting,test] 스토리북 설치 및 테일윈드 환경기반에서 스토리북을 사용할 수 있게 구성, 스토리북 테스트 코드 구성

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,29 +2,23 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true,
+    node: true
   },
   overrides: [],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module',
+    sourceType: 'module'
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/jsx-runtime',
-    'next/core-web-vitals',
-  ],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react/jsx-runtime', 'next/core-web-vitals', 'plugin:storybook/recommended'],
   plugins: ['react', '@typescript-eslint', 'react-hooks'],
   rules: {
     'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
+    'react-hooks/exhaustive-deps': 'warn'
   },
   settings: {
     react: {
-      version: 'detect',
-    },
-  },
-}
+      version: 'detect'
+    }
+  }
+};
