c4081d62e38fdc57a6349021859cd1ca1c0b1b59
Ji-yeon CHOI <jurneyx2@gmail.com>
2022-12-09 20:04:07 +0900

    [setting] 허스키 설치중

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  return <h2 className="bg-red-500">테스트입니다.</h2>
+  return <h2 className="bg-red-500">테스트입니다.</h2>;
 }
