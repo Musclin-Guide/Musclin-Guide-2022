02f58421817bf0ad850674f90f88f27033f16cd2
Ji-yeon CHOI <jurneyx2@gmail.com>
2022-12-10 22:48:31 +0900

    [setting]tailwind 커스텀 컬러, 폰트 config 추가

---
+++ src/components/Button_test/Button_test.stories.tsx
@@ -0,0 +1,127 @@
+import * as React from 'react';
+import { Story, Meta } from '@storybook/react';
+import { Button_Test } from './Button_test';
+
+export default {
+  title: 'Components/Button',
+  component: Button_Test,
+  argTypes: { onClick: { action: 'clicked' } },
+} as Meta;
+
+const Template: Story = (args) => (
+  <Button_Test
+    className={args.className}
+    variant={args.variant}
+    color={args.color}
+    size={args.size}
+    disabled={args.disabled}
+    onClick={args.onClick}
+  >
+    {args.text}
+  </Button_Test>
+);
+
+export const ButtonStory = Template.bind({});
+ButtonStory.storyName = 'Button';
+ButtonStory.args = {
+  className: '',
+  variant: 'contained',
+  color: 'primary',
+  size: 'medium',
+  disabled: false,
+  text: 'Button',
+};
+
+export const ButtonVariationsStory = () => {
+  return (
+    <div className="card max-w-800 p-3">
+      <h1 className="text-2xl font-pretendard font-semibold mb-2">Buttons</h1>
+
+      <h2 className="text-xl mt-6">Contained Buttons</h2>
+      <div className="mt-1">
+        <Button_Test>Default</Button_Test>
+        <Button_Test className="ml-2" color="primary">
+          Primary
+        </Button_Test>
+        <Button_Test className="ml-2" color="secondary">
+          Secondary
+        </Button_Test>
+        <Button_Test className="ml-2" disabled>
+          Disabled
+        </Button_Test>
+      </div>
+
+      <h2 className="text-xl mt-6">Outlined Buttons</h2>
+      <div className="mt-1">
+        <Button_Test variant="outlined">Default</Button_Test>
+        <Button_Test className="ml-2" variant="outlined" color="primary">
+          Primary
+        </Button_Test>
+        <Button_Test className="ml-2" variant="outlined" color="secondary">
+          Secondary
+        </Button_Test>
+        <Button_Test className="ml-2" variant="outlined" disabled>
+          Disabled
+        </Button_Test>
+      </div>
+
+      <h2 className="text-xl mt-6">Sizes</h2>
+      <div className="mt-1">
+        <Button_Test variant="contained" color="primary" size="small">
+          Small
+        </Button_Test>
+        <Button_Test
+          className="ml-2"
+          variant="contained"
+          color="primary"
+          size="medium"
+        >
+          Medium
+        </Button_Test>
+        <Button_Test
+          className="ml-2"
+          variant="contained"
+          color="primary"
+          size="large"
+        >
+          Large
+        </Button_Test>
+      </div>
+      <div className="mt-2">
+        <Button_Test variant="outlined" color="primary" size="small">
+          Small
+        </Button_Test>
+        <Button_Test
+          className="ml-2"
+          variant="outlined"
+          color="primary"
+          size="medium"
+        >
+          Medium
+        </Button_Test>
+        <Button_Test
+          className="ml-2"
+          variant="outlined"
+          color="primary"
+          size="large"
+        >
+          Large
+        </Button_Test>
+      </div>
+
+      <h2 className="text-xl mt-6">Link Buttons</h2>
+      <div className="mt-2">
+        <a className="btn-link" href="https://amazon.com">
+          Amazon
+        </a>
+        <a className="ml-2 btn-link" href="https://apple.com">
+          Apple
+        </a>
+        <a className="ml-2 btn-link" href="https://store.google.com">
+          Google
+        </a>
+      </div>
+    </div>
+  );
+};
+ButtonVariationsStory.storyName = 'Button Variations';
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,106 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = {
+  black: '#000000',
+  white: '#ffffff',
+  gray: {
+    50: '#fafafa',
+    100: '#f5f5f5',
+    200: '#eeeeee',
+    300: '#e0e0e0',
+    400: '#bdbdbd',
+    500: '#9e9e9e',
+    600: '#757575',
+    700: '#616161',
+    800: '#424242',
+    850: '#303030',
+    900: '#212121',
+  },
+  blue: {
+    50: '#dceefb',
+    100: '#b6e0fe',
+    200: '#84c5f4',
+    300: '#62b0e8',
+    400: '#4098d7',
+    500: '#2680c2',
+    600: '#186faf',
+    700: '#0f609b',
+    800: '#0a558c',
+    900: '#003e6b',
+  },
+  orange: {
+    50: '#fff7ed',
+    100: '#ffedd5',
+    200: '#fed7aa',
+    300: '#fdba74',
+    400: '#fb923c',
+    500: '#f97316',
+    600: '#ea580c',
+    700: '#c2410c',
+    800: '#9a3412',
+    900: '#7c2d12',
+  },
+  amber: {
+    50: '#fffbeb',
+    100: '#fef3c7',
+    200: '#fde68a',
+    300: '#fcd34d',
+    400: '#fbbf24',
+    500: '#f59e0b',
+    600: '#d97706',
+    700: '#b45309',
+    800: '#92400e',
+    900: '#78350f',
+  },
+  yellow: {
+    50: '#fffbea',
+    100: '#fff3c4',
+    200: '#fce588',
+    300: '#fadb5f',
+    400: '#f7c948',
+    500: '#f0b429',
+    600: '#de911d',
+    700: '#cb6e17',
+    800: '#b44d12',
+    900: '#8d2b0b',
+  },
+  neutral: {
+    50: '#f0f4f8',
+    100: '#d9e2ec',
+    200: '#bcccdc',
+    300: '#9fb3c8',
+    400: '#829ab1',
+    500: '#627d98',
+    600: '#486581',
+    700: '#334e68',
+    800: '#243b53',
+    900: '#102a43',
+  },
+};
+
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: colors.gray,
+        primary: {
+          ...colors.orange,
+          default: colors.orange['500'],
+        },
+        secondary: {
+          ...colors.amber,
+          default: colors.amber['400'],
+        },
+        neutral: {
+          ...colors.neutral,
+          default: colors.neutral['900'],
+        },
+      },
+      fontFamily: {
+        pretendard: ['pretendard', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
-  plugins: [],
-}
+};
