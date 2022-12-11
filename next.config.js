d90b96e53005448ed840702e46c5401ce5130af4
Ji-yeon CHOI <jurneyx2@gmail.com>
2022-12-11 22:33:53 +0900

    [chore, test] @svgr/webpack & url-loader 설치 및 config 수정 index.tsx에 테스트코드 작성

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$$/i,
+      issuer: /\.[jt]sx?$/i,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
 };
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
 // import Head from 'next/head';
 import Image from 'next/image';
-import vercelImg from '@public/vercel.svg';
+import Vercel from '@public/vercel copy.svg';
+import test from '@public/addon.png';
 
 export default function Home() {
   return (
     <>
       <h2 className="bg-primary-default"> tailwindcss 테스트 h2</h2>
       <h2>아래는 이미지 테스트 입니다.</h2>
-      <Image src={vercelImg} alt="Vercel Logo" width={72} height={16} />
+      <Vercel />
+      <Image src={test} alt="Vercel Logo" width={500} height={300} />
     </>
   );
 }
