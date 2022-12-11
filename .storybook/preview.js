b8f1b4a0ddc2c96bc10454cb73a4d6e612685461
Ji-yeon CHOI <jurneyx2@gmail.com>
2022-12-12 03:59:26 +0900

    [setting] 접근성 결과를 한국어로 출력하기위한 스토리북 ally addon config 수정 #7

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,7 @@ import 'tailwindcss/tailwind.css';
 import * as NextImage from 'next/image';
 import { RouterContext } from 'next/dist/shared/lib/router-context';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+import ko from 'axe-core/locales/ko.json';
 
 NextImage.defaultProps = {
   unoptimized: true,
@@ -21,6 +22,7 @@ export const parameters = {
   },
   a11y: {
     config: {
+      locate: ko,
       rules: [
         {
           id: 'color-contrast',
