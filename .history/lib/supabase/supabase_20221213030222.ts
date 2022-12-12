515184028f0ed2cb8b26e709a720754a6607061f
Ji-yeon CHOI <jurneyx2@gmail.com>
2022-12-13 03:16:37 +0900

    [chore] react-icons classnames 패키지 추가 #7

--- .history/lib/supabase/supabase_20221213030222.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://jpsetimmxjawidfcyvlk.supabase.co';
-const supabaseKey:string = process.env.SUPABASE_KEY || '';
-const supabase = createClient(supabaseUrl, supabaseKey);
--- .history/lib/supabase/supabase_20221213030247.ts
+++
@@ -1,4 +0,0 @@
-import { createClient, SupabaseClient } from '@supabase/supabase-js';
-const supabaseUrl = 'https://jpsetimmxjawidfcyvlk.supabase.co';
-const supabaseKey:string = process.env.SUPABASE_KEY || '';
-const supabase = createClient(supabaseUrl, supabaseKey);
