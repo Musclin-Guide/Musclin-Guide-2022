ea801e43e37b467c882985af523a0fc6889ba83b
Ji-yeon CHOI <jurneyx2@gmail.com>
2022-12-31 13:08:17 +0900

    [featur] MypageSubTabbar 개발 완료 #19

--- src/components/MypageSubBar/MypageSubBar.stories.tsx
+++ src/components/MypageSubBar/MypageSubBar.stories.tsx
@@ -1,17 +1,23 @@
 import { Story, Meta } from '@storybook/react';
-import { MyPageButton } from '@components/MypageSubBar/MypageSubBar';
+import { MyPagSubBar } from '@components/MypageSubBar/MypageSubBar';
 import { addFigmaToken } from '@utils/addFigmaToken';
 
 export default {
   title: 'Components/Mypage',
-  component: MyPageButton,
+  component: MyPagSubBar,
   // argTypes: { onClick: { action: 'clicked' } },
 } as Meta;
 
-const Template: Story = (args) => <MyPageButton>{args.content}</MyPageButton>;
+const Template: Story = (args) => <MyPagSubBar></MyPagSubBar>;
 
-export const SubTabButton = Template.bind({});
-SubTabButton.storyName = 'SubButton';
-SubTabButton.args = {
-  content: '테스트',
+export const SubTabBar = Template.bind({});
+SubTabBar.storyName = 'MyPageSubBar';
+SubTabBar.args = {
+  className: '',
+  text: '테스트',
 };
+
+addFigmaToken(
+  SubTabBar,
+  'https://www.figma.com/file/9Q9d6I31A1lMp11UMLZjA1/wireFrames(1st-Cycle)?node-id=132%3A12448&t=z7FiGGI5b6rRjKgE-4'
+);
