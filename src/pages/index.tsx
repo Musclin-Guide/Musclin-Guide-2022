10b76168bedcfdcd69b4f24c6e4e0b65d458ac21
Ji-yeon CHOI <jurneyx2@gmail.com>
2023-01-02 17:22:04 +0900

    [setting,chore] atom폴더 생성(알리아스 포함) 및 _app.tsx에서 리코일 사용 환경 구성

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,4 @@
-import Image from 'next/image';
 import Head from 'next/head';
-import SVGICON from '@pages/test';
-import addon from '@public/addon.png';
 
 export default function Home() {
   return (
@@ -11,8 +8,6 @@ export default function Home() {
       </Head>
       <h2 className="bg-primary-default"> tailwindcss 테스트 h2</h2>
       <h2>아래는 이미지 테스트 입니다.</h2>
-      <SVGICON />
-      <Image src={addon} alt="Vercel Logo" width={500} height={300} />
     </>
   );
 }
