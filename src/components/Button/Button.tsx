0f432be3b52cc34be09e17f01f68acafde341539
Lee <dltmdgns0508@gmail.com>
2023-01-04 17:37:24 +0900

    [fix] Import React 제거

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,4 @@
 import clsx from 'clsx';
-import React from 'react';
 import { IconContext } from 'react-icons';
 import { HiMail } from 'react-icons/hi';
 import buttonStyles from './Button.module.css';
