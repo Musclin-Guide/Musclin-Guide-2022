6dca606a4f77580b609050f010f9cfffc025a321
Ji-yeon CHOI <jurneyx2@gmail.com>
2023-01-04 18:13:51 +0900

    [feat] FeaturedSlideList 완성 #28

--- src/components/FeaturedSlide/FeaturedSlideList.tsx
+++ src/components/FeaturedSlide/FeaturedSlideList.tsx
@@ -1,25 +1,50 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
-import 'swiper/css';
-import photo from '@assets/activated_false,png';
 import Image from 'next/image';
+import styles from '@components/FeaturedSlide/FeaturedSlideList.module.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Keyboard, Navigation, Pagination } from 'swiper';
+// Import Swiper styles
+import 'swiper/css/bundle';
+
 export const FeaturedSlideList = () => {
+  const demoImgList = [
+    {
+      id: 1,
+      src: '/assets/activated_true.png',
+      alt: '유저가 업로드한 이미지',
+    },
+    {
+      id: 2,
+      src: '/assets/activated_true.png',
+      alt: '유저가 업로드한 이미지',
+    },
+    {
+      id: 3,
+      src: '/assets/activated_true.png',
+      alt: '유저가 업로드한 이미지',
+    },
+  ];
+  SwiperCore.use([Navigation, Pagination, Keyboard]);
   return (
-    <Swiper
-      spaceBetween={50}
-      slidesPerView={3}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
-    >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>
-        <Image src={''} alt={''} />
-        <img src={photo} alt="" />
-      </SwiperSlide>
-      ...
-    </Swiper>
+    <>
+      <Swiper
+        spaceBetween={1}
+        slidesPerView={1}
+        // rewind={true}
+        onSlideChange={() => console.log('slide change')}
+        // onSwiper={(swiper) => console.log(swiper)}
+        pagination={{ clickable: true }}
+        keyboard={true}
+        centeredSlides={true}
+        centeredSlidesBounds={true}
+      >
+        {demoImgList.map((i) => {
+          return (
+            <SwiperSlide key={`${i.id}`} className={styles.swiperslide}>
+              <Image src={i.src} width={360} height={360} alt={i.alt} />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </>
   );
 };
