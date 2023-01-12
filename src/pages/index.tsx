cdb24f86769c77177a9bd48b8b8e72b188cd56c0
Lee <dltmdgns0508@gmail.com>
2023-01-13 04:41:42 +0900

    [feat] 기본 구조 및 스타일 적용

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,10 @@
 import Head from 'next/head';
+import { FeaturedSlideList } from '@components/FeaturedSlide';
+import { Button } from '@components/Button';
+import { ImagedListItem } from '@components/ImagedListItem';
+import { Header } from '@components/Header';
+import { TabBar } from '@components/TabBar';
+import styles from '@pages/homepage.module.css';
 
 export default function Home() {
   return (
@@ -11,8 +17,96 @@ export default function Home() {
           sizes="16x16"
         />
       </Head>
-      <h2 className="bg-primary-default"> tailwindcss 테스트 h2</h2>
-      <h2>아래는 이미지 테스트 입니다.</h2>
+      <div className={styles.FixedContents}>
+        <Header></Header>
+        <TabBar></TabBar>
+      </div>
+      <main className={styles.Main}>
+        <div className={styles.Contents}>
+          <section>
+            <FeaturedSlideList></FeaturedSlideList>
+          </section>
+          <section className={styles.Section}>
+            <div className={styles.CocktailHeader}>
+              <h2 className={styles.Title}>인기 칵테일</h2>
+              <Button color="White" size="base">
+                더보기
+              </Button>
+            </div>
+            <div>
+              <ul className={styles.Cocktail}>
+                <li>
+                  <ImagedListItem
+                    imgWrapper="Row"
+                    listWrapper="Row"
+                  ></ImagedListItem>
+                </li>
+                <li>
+                  <ImagedListItem
+                    imgWrapper="Row"
+                    listWrapper="Row"
+                  ></ImagedListItem>
+                </li>
+                <li>
+                  <ImagedListItem
+                    imgWrapper="Row"
+                    listWrapper="Row"
+                  ></ImagedListItem>
+                </li>
+              </ul>
+            </div>
+          </section>
+          <section className={styles.Section}>
+            <div className={styles.ProductInfo}>
+              <div className={styles.ProductHeader}>
+                <h2 className={styles.Title}>추천 제품</h2>
+                <Button color="White" size="base">
+                  더보기
+                </Button>
+              </div>
+              <p className={styles.InfoText}>
+                유저정보를 바탕으로 선정된 추천 제품입니다.
+              </p>
+            </div>
+            <div>
+              <ul className={styles.Product}>
+                <li>
+                  <ImagedListItem
+                    contentsStyle="Col"
+                    wrapperStyle="Col"
+                    imgWrapper="Col"
+                    listWrapper="Col"
+                  ></ImagedListItem>
+                </li>
+                <li>
+                  <ImagedListItem
+                    contentsStyle="Col"
+                    wrapperStyle="Col"
+                    imgWrapper="Col"
+                    listWrapper="Col"
+                  ></ImagedListItem>
+                </li>
+                <li>
+                  <ImagedListItem
+                    contentsStyle="Col"
+                    wrapperStyle="Col"
+                    imgWrapper="Col"
+                    listWrapper="Col"
+                  ></ImagedListItem>
+                </li>
+                <li>
+                  <ImagedListItem
+                    contentsStyle="Col"
+                    wrapperStyle="Col"
+                    imgWrapper="Col"
+                    listWrapper="Col"
+                  ></ImagedListItem>
+                </li>
+              </ul>
+            </div>
+          </section>
+        </div>
+      </main>
     </>
   );
 }
