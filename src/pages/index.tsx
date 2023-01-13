7db702b3e3ea0ed20c0ef01eee9b8af2a3a6386c
Ji-yeon CHOI <jurneyx2@gmail.com>
2023-01-14 03:11:32 +0900

    [test] index.tsx 테스트 코드 삭제 #62

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,6 @@ export default function Home() {
       </Head>
       <h2 className="bg-primary-default"> tailwindcss 테스트 h2</h2>
       <h2>아래는 이미지 테스트 입니다.</h2>
-      <TabBar />
     </>
   );
 }
