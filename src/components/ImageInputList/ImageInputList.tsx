e16437f097adf309d49cbc2ccfaf59c865ea3596
Lee <dltmdgns0508@gmail.com>
2023-01-19 00:56:12 +0900

    [feat] ImageInputList 컴포넌트 FormData 방식으로 변경

--- src/components/ImageInputList/ImageInputList.tsx
+++ src/components/ImageInputList/ImageInputList.tsx
@@ -1,24 +1,14 @@
 import { ImageInputButton } from '@components/ImageInputList/ImageInputButton/ImageInputButton';
 import InputListStyle from '@components/ImageInputList/ImageInputList.module.css';
 import clsx from 'clsx';
-import { useMemo } from 'react';
 import { useEffect, useState } from 'react';
-import { Dispatch, SetStateAction } from 'react';
-interface ImageObject {
-  key: string;
-  value: File;
-}
 interface ImageInputListProps {
-  // images: ImageObject[] | undefined;
-  // setImages: Dispatch<SetStateAction<ImageObject[] | undefined>>;
   formData: FormData;
   size?: 'primary' | 'small';
   remit?: number;
 }
 
 export const ImageInputList = ({
-  // images,
-  // setImages,
   formData,
   size = 'primary',
   remit = 4,
@@ -27,31 +17,28 @@ export const ImageInputList = ({
     InputListStyle.ImageInputList,
     size === 'primary' ? InputListStyle.Primary : InputListStyle.Small
   );
-  const data = useMemo(() => {
-    const values = formData.values();
-    return [...values];
+  const [datas, setDatas] = useState<File[]>();
+  useEffect(() => {
+    formData?.forEach((value) => {
+      setDatas((current) => {
+        return current ? [...current, value as File] : [value as File];
+      });
+    });
   }, [formData]);
   return (
     <div className={InputListClassName}>
-      {data?.map((value) => (
+      {datas?.map((file) => (
         <ImageInputButton
-          key={value.name}
-          file={value}
+          key={file.name}
+          file={file}
           size={size}
           formData={formData}
+          setDatas={setDatas}
         />
       ))}
-      {((data && data.length < remit) || !data) && (
-        <ImageInputButton size={size} formData={formData} />
+      {((datas && datas.length < remit) || !datas) && (
+        <ImageInputButton size={size} formData={formData} setDatas={setDatas} />
       )}
-      <button
-        className={'bg-red-600 w-14 h-14 top-72 fixed'}
-        onClick={() => {
-          console.log([...formData.values()]);
-        }}
-      >
-        test
-      </button>
     </div>
   );
 };
