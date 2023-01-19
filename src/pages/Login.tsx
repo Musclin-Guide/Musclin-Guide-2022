6695031750122540114eac2df4baa406442c3af2
Lee <dltmdgns0508@gmail.com>
2023-01-20 03:12:30 +0900

    [feat] LoginPage 기본 뼈대 생성

---
+++ src/pages/Login.tsx
@@ -0,0 +1,50 @@
+import { Layout } from '@components/Layout/Layout';
+import { Button } from '@components/Button';
+import { TextInput } from '@components/TextInput';
+import { useState } from 'react';
+import { HiKey } from 'react-icons/hi';
+import { ALinkMenuItem as ALink } from '@components/index';
+import style from '@pages/Login.module.css';
+
+export const Login = (): JSX.Element => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  return (
+    <Layout>
+      <h2 className={style.Header}>로그인</h2>
+      <form>
+        <TextInput
+          label="email"
+          type="email"
+          placeHolder="이메일을 입력해 주세요"
+          value={email}
+          setValue={setEmail}
+          required
+        ></TextInput>
+        <TextInput
+          label="password"
+          type="password"
+          placeHolder="비밀번호를 입력해 주세요"
+          value={password}
+          setValue={setPassword}
+          required
+        ></TextInput>
+      </form>
+      <Button
+        className={'w-full'}
+        size="xl"
+        icon={{ type: 'Leading', element: <HiKey></HiKey> }}
+      >
+        로그인
+      </Button>
+      <div>
+        <ALink href="/password" isExternal={false}>
+          비밀번호 찾기
+        </ALink>
+        <ALink href="/register" isExternal={false}>
+          회원 가입
+        </ALink>
+      </div>
+    </Layout>
+  )
+}
