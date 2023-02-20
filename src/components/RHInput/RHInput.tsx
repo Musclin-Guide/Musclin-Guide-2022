5549fcceba7e1634d5b662b9940c37f74eed99ca
cinos81 <cinos81@gmail.com>
2023-02-20 21:29:18 +0900

    [feat] RHInput이 벨리데이션 룰을 받도록 수정

--- src/components/RHInput/RHInput.tsx
+++ src/components/RHInput/RHInput.tsx
@@ -1,28 +1,33 @@
 import { InputHTMLAttributes, PropsWithChildren } from 'react';
 import { Path, useController, useFormContext } from 'react-hook-form';
+import { RegisterOptions } from 'react-hook-form/dist/types/validator';
 
-interface IRHInput<FormType> extends InputHTMLAttributes<HTMLInputElement> {
+interface IRHInput<FormType extends object>
+  extends InputHTMLAttributes<HTMLInputElement> {
   name: Path<FormType>;
+  rules?: RegisterOptions<FormType>;
 }
 
 export function RHInput<FormType extends object>({
   name,
-  className,
+  rules,
+  ...rest
 }: PropsWithChildren<IRHInput<FormType>>) {
   const { control } = useFormContext<FormType>();
   const { field } = useController({
     name,
     control,
+    rules,
   });
 
   return (
     <input
-      className={className}
       name={field.name}
       onChange={field.onChange}
       onBlur={field.onBlur}
       value={field.value}
       ref={field.ref}
+      {...rest}
     />
   );
 }
